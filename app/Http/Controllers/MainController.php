3462afb39124fa472d00215719417b164d6fbb45
Rémi <remi@MacBook-Air-de-Remi.local>
2020-08-10 16:11:37 +0200

    commit controller

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    //methode pour la page d'accueil
+    public function home(){
+        echo 'coucou';
+    }
+}
